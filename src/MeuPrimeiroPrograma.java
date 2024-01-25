b846a78e15945923fbdb9931836bb4363d6339b5
juan <juanauer2019@gmail.com>
2024-01-25 13:43:56 -0300

    variaveis

---
+++ src/MeuPrimeiroPrograma.java
@@ -0,0 +1,18 @@
+public class MeuPrimeiroPrograma {
+
+    public  static void main(String[] args) {
+        MinhaInstrucaoDeImprimir();
+    }
+
+    static void MinhaInstrucaoDeImprimir() {
+        var Nome = "Fabio";  //com var nao preciso tipar as variaveis
+        // tipo de dados
+        String Texto = "ola a todos";
+        int numero = 1;
+        double percentual = 1.12;
+        boolean eVerdadeiro = true;
+
+        System.out.println(Texto);
+    }
+
+}
